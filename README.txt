b2937d2468858a8d7d42d13ed7c3c57ff56d09ed
Billy Armfield <billy.armfield@gmail.com>
2018-06-20 17:17:51 -0400

    added mlp classifier to program and updated README

--- README.txt
+++ README.txt
@@ -1,8 +1,30 @@
-This is a simple tree classifier app written in python. This overly simplistic
-model will predict the gender of an individual based on three features:
+This program will train 3 classifiers: a decision tree, support vector
+machine, and and multi-layer perceptron network to make predictions about the
+gender of an individual. malefemale.py is intended to be run from the command
+line. 
 
-- Height
-- Weight
-- Shoe size
+Features (X): height, weight, shoe size
 
-This is a WIP, so any and all comments/advice are welcome!
+Label (Y): female, male
+
+
+/----------RESULTS----------/
+The decision tree model is the only one that selects correctly from user input
+data. One major issue may be the limited number of examples. More exploration
+is required to better understand the three models. 
+
+
+
+
+The models are drawn from scikit-learn version 0.19.1.
+
+A special thanks to Siraj Naval for the tutorial and inspiration (link below).
+
+Sources/credit
+--------------
+Siraj Raval: https://www.youtube.com/channel/UCWN3xxRkmTPmbKwht9FuE5A
+CDC: https://www.cdc.gov/nchs/fastats/body-measurements.htm
+Scikit-learn.org:
+Tree: http://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
+SVM: http://scikit-learn.org/stable/modules/svm.html#classification
+MLP: http://scikit-learn.org/stable/modules/neural_networks_supervised.html#neural-networks-supervised
--- malefemale.py
+++ malefemale.py
@@ -1,29 +1,58 @@
 from sklearn import tree
+from sklearn import svm
+from sklearn.neural_network import MLPClassifier as mlp
 
-# features
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], [159, 59, 37], [171, 75, 42], [181, 85, 43]]
+# features: height, weight, shoe size
+X = [[70., 195., 10.], [73., 185., 11.], [63., 168., 7.], [60., 150., 8.], [65., 180., 9.], [73., 162., 11.], [65., 126., 7.], [63., 145., 7.], [62., 150., 9.], [68., 175., 9.], [66., 200., 12.]]
 
 # labels
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
-# DecisionTreeClassifier() initializes model as our classifier
-model = tree.DecisionTreeClassifier()
 
-# fit() trains the model on our data, enabling it to make predictions
-model = model.fit(X, Y)
+# /-------------Decisiion Tree--------------/
+# DecisionTreeClassifier() initializes tree_model as our classifier and
+# fit() trains the model on our features and labels, enabling it to make 
+# predictions
+tree_model = tree.DecisionTreeClassifier()
+tree_model = tree_model.fit(X, Y)
+
+
+# /----------Support Vector Machine----------/
+# SVC() initializes svm_model as a classifier and fit()
+svm_model = svm.SVC()
+svm_model = svm_model.fit(X, Y)
+
+
+# /----------Multi-layer Perceptron---------/
+mlp_model = mlp(solver='lbfgs', alpha=1e-5, 
+				hidden_layer_sizes=(5, 2), random_state=1)
+mlp_model = mlp_model.fit(X, Y)
 
 
 # ask for user input
-print("Enter 3 parameters to predict gender: ")
-params = list()
+print("\nEnter height, weight, and shoe size to predict gender... ")
+param_vals = list()
+params = ['Height (in inches)', 'Weight', 'Shoe size']
 
-for i in range(3):
-	n = input("Enter parameter {}: ".format(i+1))
-	params.append(int(n))
+while True:
+	print()
+	param_vals.clear()
+	for param in params:
+		n = input("{}: ".format(param))
+		param_vals.append(float(n))
 
-# the predict() method will use the model to guess the class of the input
-# prediction = model.predict([[190, 70, 43]])
-prediction = model.predict([params])
+	# the predict() method will use the model to guess the class of the input
+	# prediction = model.predict([[190, 70, 43]])
+	tree_pred = tree_model.predict([param_vals])
+	svm_pred = svm_model.predict([param_vals])
+	mlp_pred = mlp_model.predict([param_vals])
 
-print("Prediction: {}".format(prediction))
+	print("\nPrediction using decision tree: {}".format(tree_pred))
+	print("Prediction using support vector machine: {}".format(svm_pred))
+	print("Prediction using multi-layer perceptron: {}".format(mlp_pred))
+	print()
+
+	loop = input("Enter y to ask again...  ")
+	if loop.lower() != "y":
+		break
